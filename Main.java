1442b2ba97d48777202faef72cad7a5184005150
Katerina Walter <94501766+KaterinaWalter@users.noreply.github.com>
2026-02-24 16:15:54 +0000

    4.8 notes pt. 1

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Main {
 
    public static void main(String []args) {
@@ -22,6 +24,40 @@ public class Main {
       int parsedI = Integer.parseInt(stringyNums2);
       System.out.println(parsedI);
 
+      // 4.8 ARRAYLIST
+      /* ArrayList is a RE-SIZABLE collection
+         You can ADD or REMOVE items from lists!
+         ArrayList is MUTABLE (can change during run)
+       */
+
+      // ArrayList<Type> name = new ArrayList<Type>();
+      ArrayList<String> toDo = new ArrayList<String>();
+      // For numbers, must use WRAPPER classes (not primitives!)
+      ArrayList<Integer> intList = new ArrayList<Integer>();
+      ArrayList<Double> doubList = new ArrayList<Double>();
+
+      // ARRAYLIST METHODS (assume "list" is an ArrayList)
+
+      // list.size() returns an int 
+      // counts the number of items in the list
+      int numTasks = toDo.size();
+      System.out.println("Number of items on To Do list: " + numTasks);
+
+      // list.add( obj ) appends the obj to end of list
+      // returns a boolean, but that's not important
+      toDo.add( "Buy cat food" );
+      toDo.add( "Finish reading" );
+      toDo.add( "Call mom" );
+      // check size after adding stuff
+      System.out.println("Number of items after adding: " + toDo.size() ); 
+      System.out.println( toDo ); // ArrayList has a nice toString method!!!
+
+      // list.add( index, obj ) inserts the obj 
+      // AT a specific INDEX! (position)
+      toDo.add( 1, "Grade homework" );
+      System.out.println( toDo ); 
+      // REMEMBER that indexes start at 0
+      // so 1 means we added to the SECOND spot
 
    }
 }
