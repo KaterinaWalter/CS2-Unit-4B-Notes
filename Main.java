444ffd8ca6d31b777c801c884ea2e9c2d3517a3c
Katerina Walter <94501766+KaterinaWalter@users.noreply.github.com>
2026-03-03 15:25:31 +0000

    4.9 notes

--- Main.java
+++ Main.java
@@ -79,6 +79,61 @@ public class Main {
       System.out.println( toDo ); 
       System.out.println("We removed the item: " + removedItem);
 
+      // 4.9 ARRAYLIST TRAVERSAL
+      ArrayList<Integer> scores = new ArrayList<Integer>();
+      scores.add(22);
+      scores.add(45);
+      scores.add(78);
+      System.out.println(scores); 
+
+      // ENHANCED FOR-EACH LOOP
+      // to "visit" every item in order
+      // does not keep track of current index/position
+      for ( Integer currentScore : scores ) {
+         System.out.println("Test Score: " + currentScore); 
+      }
+      // typical example: count up all items
+      double sum = 0;
+      for ( Integer score : scores ) {
+         sum = sum + score; // OR sum += score;
+      }
+      double avg = sum / scores.size();
+      System.out.println("Test Average: " + avg);
+
+      
+      // STANDARD FOR LOOP: when you need to keep
+      // track of the current INDEX (position)
+      // like if you want to use a method (set, get, etc.)
+      for (int i = 0; i < scores.size(); i++ ) {
+         // get item at current index
+         int currentItem = scores.get(i);
+         int curvedScore = currentItem + 100;
+         scores.set(i, curvedScore); // replace item
+      }
+      System.out.println(scores);
+
+      // WHILE LOOP (iterate until condition is false)
+      ArrayList<String> groceries = new ArrayList<String>();
+      groceries.add("penne");
+      groceries.add("cheddar");
+      groceries.add("penne");
+      groceries.add("nutella");
+      // guess what this loop is trying to do?
+      boolean found = false;
+      int index = 0;
+      // !found is the same as found == false
+      while ( index < groceries.size() && !found ) {
+         if ( "penne".equals( groceries.get(index) ) ) {
+            // if current item in list is "penne"
+            groceries.remove(index);
+            found = true;
+         }
+         else {
+            index++; // continue through list
+         }
+      }
+      System.out.println(groceries);
+
 
    } // END MAIN METHOD
 } // END CLASS
